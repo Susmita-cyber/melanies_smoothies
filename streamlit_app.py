2e6b1c8b315a51e950b866f73c2951830f782047
Susmita Chakraborty <61580468+Susmita-cyber@users.noreply.github.com>
2024-07-01 16:51:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col;
+from snowflake.snowpark.functions import col
 import requests
 import pandas as pd 
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
