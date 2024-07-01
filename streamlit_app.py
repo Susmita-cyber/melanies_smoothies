4d07cf28ba5972f842066779052b9db32c26a3d9
Susmita Chakraborty <61580468+Susmita-cyber@users.noreply.github.com>
2024-07-01 18:18:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,34 +21,26 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:',
-    pd_df['FRUIT_NAME'],
-    max_selections=5
-)
+    'Choose up to 5 ingredients:'
+    ,my_dataframe,
+    ,max_selections=5
+    )
 
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write(f'The search value for {fruit_chosen} is {search_on}.')
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
-        st.subheader(f'{fruit_chosen} Nutrition Information')
-        fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen.lower()}")
+        st.subheader (fruit_chosen+' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        st.write (ingredients_string)
         
-        if fruityvice_response.status_code == 200:
-            fv_data = fruityvice_response.json()
-            fv_df = pd.DataFrame([fv_data])
-            st.dataframe(fv_df, use_container_width=True)
-        else:
-            st.error(f"Failed to fetch data for {fruit_chosen}")
-
-    st.write(ingredients_string)
     
-    my_insert_stmt = f"""
-    INSERT INTO smoothies.public.orders(ingredients, name_on_order)
-    VALUES ('{ingredients_string}', '{name_on_order}')
-    """
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+               values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
 
     st.write(my_insert_stmt)
     
