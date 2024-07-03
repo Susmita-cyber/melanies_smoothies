233abb0cfbb56015465def9a9b42501da2cfb20c
Susmita Chakraborty <61580468+Susmita-cyber@users.noreply.github.com>
2024-07-03 17:13:05 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ ingredients_list = st.multiselect(
 if ingredients_list:
      ingredients_string =''
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+            ingredients_string += fruit_chosen + ' '
    
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -34,10 +34,11 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     #st.stop()
     
-    time_to_insert = st.button('Submit Order')
-    if time_to_insert:
+   time_to_insert = st.button('Submit Order')
+   if time_to_insert:
        session.sql(my_insert_stmt).collect()
        st.success(f'Your Smoothie is ordered,{name_on_order}!', icon="✅")
 
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-      fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
